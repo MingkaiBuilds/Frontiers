cb200c52c5981fdedfc585cc0d892b5fbb806e03
Mingkai <mingkai.builds@gmail.com>
2026-08-12 03:21:51 -0700

    Connect site to engineering discussions

--- src/data/site.ts
+++ src/data/site.ts
@@ -2,3 +2,9 @@ export const SITE_NAME = 'Physical Extremes';
 export const GITHUB_REPOSITORY_URL = 'https://github.com/MingkaiBuilds/Frontiers';
 export const DISCUSSIONS_URL = `${GITHUB_REPOSITORY_URL}/discussions`;
 
+export const DISCUSSION_URLS = {
+  reportQuestions: `${DISCUSSIONS_URL}/new?category=report-q-a`,
+  technicalCritique: `${DISCUSSIONS_URL}/new?category=technical-critique`,
+  investigationProposal: `${DISCUSSIONS_URL}/new?category=investigation-proposals`,
+  buildLog: `${DISCUSSIONS_URL}/new?category=build-logs`,
+} as const;
